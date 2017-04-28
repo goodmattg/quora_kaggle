25ea852953483c7a44c7d1b60f2b152ffd25507d
goodmattg <mattgoodman13@gmail.com>
2017-04-27 23:08:48 -0400

    modifications to data storage script

--- STORE_DATA.py
+++ STORE_DATA.py
@@ -5,6 +5,7 @@
 
 import pandas as pd
 from pycorenlp import StanfordCoreNLP 
+from math import ceil
 import TreeBuild as tb
 import pickle
 import json, os
@@ -17,6 +18,7 @@ nlp = StanfordCoreNLP('http://localhost:9000')
 def read_data(path_to_file):
     df = pd.read_csv(path_to_file)
     print ("Shape of base training File = ", df.shape)
+    df.dropna(inplace=True)
     print("Shape of base training data after cleaning = ", df.shape)
     return df
 
@@ -32,78 +34,81 @@ def _getNLPToks_(rawSentence):
             'outputFormat': 'json'
         })
     except UnicodeDecodeError:
+        print("HERE")
         sentence = unidecode(rawSentence)
         output = nlp.annotate(sentence, properties={
             'annotators': 'tokenize,ssplit,pos,parse,depparse',
             'outputFormat': 'json'
         })
+    if (isinstance(output, str)):
+        output = json.loads(output) # Convert str output to dict
+        
     dependencies = output['sentences'][0]['basicDependencies']
     tokens = output['sentences'][0]['tokens']
     parse = output['sentences'][0]['parse'].split("\n")
-    return {'deps':dependencies, 'toks':tokens, 'parse':parse}
-
-
-# In[ ]:
+    
+    return {'deps':dependencies,
+            'toks':tokens, 
+            'parse':parse}
 
 
 if __name__ == "__main__":    
-    with open('stanfordData_train.nlp', 'wb') as fout:
-        count = 0
-        for row in df_train.iterrows():
-            try:
-                q1_stanford = _getNLPToks_(row[1]['question1'])
-                q2_stanford = _getNLPToks_(row[1]['question2'])
+    
+    block_ctr = 1        
+    count = 0
 
-                tree_1 = tb.tree()
-                tree_2 = tb.tree()
+    fout = open('stanfordData_train' + block_ctr + '.nlp', 'wb') 
+      
+    for row in df_train.iterrows():
+        try:
+            q1_stanford = _getNLPToks_(row[1]['question1'])
+            q2_stanford = _getNLPToks_(row[1]['question2'])
 
-                # Generate a tree structure
-                tb._generateTree_(q1_stanford['parse'], tree_1)
-                tb._generateTree_(q2_stanford['parse'], tree_2)
+            tree_1 = tb.tree()
+            tree_2 = tb.tree()
 
-                # Flip the trees
-                tb._flipTree_(tree_1)
-                tb._flipTree_(tree_2)
+            # Generate a tree structure
+            tb._generateTree_(q1_stanford['parse'], tree_1)
+            tb._generateTree_(q2_stanford['parse'], tree_2)
 
-                tmp = {'q1': {
-                        'raw': row[1]['question1'],
-                        'toks': q1_stanford['toks'],
-                        'deps': q1_stanford['deps'],
-                        'parse': tree_1
-                        },
-                       'q2': {
-                        'raw': row[1]['question2'],
-                        'toks': q2_stanford['toks'],
-                        'deps': q2_stanford['deps'],
-                        'parse': tree_2                
-                        },
-                       'id':row[1]['id'],
-                       'is_duplicate':row[1]['is_duplicate']
-                       }
+            # Flip the trees
+            tb._flipTree_(tree_1)
+            tb._flipTree_(tree_2)
 
-                pickle.dump(tmp, fout, protocol=pickle.HIGHEST_PROTOCOL)
+            tmp = {'q1': {
+                    'raw': row[1]['question1'],
+                    'toks': q1_stanford['toks'],
+                    'deps': q1_stanford['deps'],
+                    'parse': tree_1
+                    },
+                   'q2': {
+                    'raw': row[1]['question2'],
+                    'toks': q2_stanford['toks'],
+                    'deps': q2_stanford['deps'],
+                    'parse': tree_2                
+                    },
+                   'id':row[1]['id'],
+                   'is_duplicate':row[1]['is_duplicate']
+                   }
 
-                tree_1.clear()
-                tree_2.clear()
-            except:
-                print("Failure on row: %d" % count)
-                
-            count+=1
+            pickle.dump(tmp, fout, protocol=pickle.HIGHEST_PROTOCOL)
+
+            tree_1.clear()
+            tree_2.clear()
+        
+        except OSError: 
+            fout.close()
+            block_ctr += 1
+            fout = open('stanfordData_train' + block_ctr + '.nlp', 'wb') 
+            pickle.dump(tmp, fout, protocol=pickle.HIGHEST_PROTOCOL)
+            tree_1.clear()
+            tree_2.clear()
+      
+        except:
+            print("Failure on row: %d" % count)
+
+        count+=1
     
     print("NLP Tree Generation completed!")
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
 
 
