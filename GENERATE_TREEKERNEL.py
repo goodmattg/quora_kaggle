ee6efc8da0230e53f44c546277b9d8a6bfd73d89
goodmattg <mattgoodman13@gmail.com>
2017-05-02 11:15:43 -0400

    misc changes to notebooks

---
+++ GENERATE_TREEKERNEL.py
@@ -0,0 +1,89 @@
+
+from pycorenlp import StanfordCoreNLP
+import TreeKernel as tk
+import TreeBuild as tb
+import numpy as np
+import pandas as pd
+import pickle
+import sys
+
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+def _getNLPToks_(rawSentence):
+    try:
+        output = nlp.annotate(rawSentence, properties={
+            'annotators': 'tokenize,ssplit,pos,parse',
+            'outputFormat': 'json'
+        })
+    except UnicodeDecodeError:
+        sentence = unidecode(rawSentence)
+        output = nlp.annotate(sentence, properties={
+            'annotators': 'tokenize,ssplit,pos,parse',
+            'outputFormat': 'json'
+        })
+    tokens = output['sentences'][0]['tokens']
+    parse = output['sentences'][0]['parse'].split("\n")
+    return {
+        'toks':tokens, 'parse':parse
+    }
+
+
+# ARGS: Path to data file, lambda, path to output
+
+if __name__ == "__main__":
+    feature_vect = {}
+
+    with open(sys.argv[1], 'rb') as handle:
+        count = 0
+        while True:
+            if (count % 10000 == 0):
+                print(count)
+            try:
+                unit = pickle.load(handle)
+                # ST Syntax score
+                (rscore_st, nscore_st) = tk._CollinsDuffy_(unit['q1']['parse'], unit['q2']['parse'], sys.argv[3], 1, 0)
+
+                feature_vect[unit['id']] = {
+                    'id':unit['id'],
+                    'cdNorm_st':nscore_st
+                }
+
+            except EOFError:
+                break
+            except:
+                print("Quote error on: %d", unit['id'])
+
+                q1_stanford = _getNLPToks_(unit['q1']['raw'].replace('"','').replace("'",''))
+                q2_stanford = _getNLPToks_(unit['q2']['raw'].replace('"','').replace("'",''))
+
+                tree_1 = tb.tree()
+                tree_2 = tb.tree()
+
+                # Generate a tree structure
+                tb._generateTree_(q1_stanford['parse'], tree_1)
+                tb._generateTree_(q2_stanford['parse'], tree_2)
+
+                # Flip the trees
+                tb._flipTree_(tree_1)
+                tb._flipTree_(tree_2)
+
+
+                (rscore_st, nscore_st) = tk._CollinsDuffy_(tree_1, tree_2, sys.argv[3], 1, 0)
+
+                feature_vect[unit['id']] = {
+                    'id':unit['id'],
+                    'cdNorm_st':nscore_st
+                }
+
+                print("Quote error resolved")
+                pass
+
+            count += 1
+
+    df_feature = pd.DataFrame.from_dict(feature_vect)
+    df_feature = df_feature.transpose()
+
+    df_feature[['id']] = df_feature[['id']].astype(int)
+
+    df_feature.to_csv(sys.argv[2], index=False)
+
